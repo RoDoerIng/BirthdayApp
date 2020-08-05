aac07d4adee8331535fbe8e2047e2f264e3e74b5
RoDoerIng <ronnydoering.ing@gmail.com>
2020-08-05 21:44:05 +0200

    Fill question 8

--- src/app/question8/question8.component.ts
+++ src/app/question8/question8.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseQuestion } from '../questions/base-question';
 
 @Component({
   selector: 'app-question8',
   templateUrl: './question8.component.html',
   styleUrls: ['./question8.component.css']
 })
-export class Question8Component implements OnInit {
-
-  constructor() { }
+export class Question8Component extends BaseQuestion implements OnInit {
 
   ngOnInit(): void {
   }
