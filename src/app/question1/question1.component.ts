783f3d6ab0e68530e6addbdbdf6d81524c26253a
RoDoerIng <ronnydoering.ing@gmail.com>
2020-08-05 21:51:27 +0200

    Fill questions 1 and 2

--- src/app/question1/question1.component.ts
+++ src/app/question1/question1.component.ts
@@ -1,23 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BatteryService } from '../services/battery.service';
+import { BaseQuestion } from '../questions/base-question';
 
 @Component({
   selector: 'app-question1',
   templateUrl: './question1.component.html',
   styleUrls: ['./question1.component.css']
 })
-export class Question1Component implements OnInit {
-
-  constructor(private router: Router, public batteryService: BatteryService) { 
-    batteryService.setBatteryLevel(100);
-  }
+export class Question1Component extends BaseQuestion implements OnInit {
 
   ngOnInit(): void {
-  }
-
-  goToQuestion2(age: number){
-    this.batteryService.setBatteryLevel(age)
-    this.router.navigateByUrl('/question2');
+    this.batteryService.setBatteryLevel(100)
   }
 }
